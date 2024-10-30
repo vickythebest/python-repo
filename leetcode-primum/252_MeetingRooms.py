84743aa21193e296a8f5567b55553aeb445dab1a
vivek kumar <vickythebest008@gmail.com>
2024-10-30 10:03:11 -0700

    leedcode primum

---
+++ leetcode-primum/252_MeetingRooms.py
@@ -0,0 +1,41 @@
+class Interval():
+    def __init__(self,start,end):
+        self.start=start
+        self.end=end
+
+    @staticmethod
+    def canAttendMeetins(self):
+        intervals.sort(key=lambda i : i.start)
+
+        for i in range(1,len(intervals)):
+            # print(intervals[i-1])
+            i1=intervals[i-1]
+            i2=intervals[i]
+
+            if i1.end > i2.start:
+                return False
+        return True
+
+# class MeetingScheduler():
+#     def __init__(self,intervals):
+#         self.intervals=[Interval(start,end) for start,end in intervals]
+        
+#     def canAttendMeetins(self):
+#         self.intervals.sort(key=lambda i : i.start)
+
+#         for i in range(1,len(self.intervals)):
+#             # print(intervals[i-1])
+#             i1=self.intervals[i-1]
+#             i2=self.intervals[i]
+
+#             if i1.end > i2.start:
+#                 return False
+#         return True
+
+
+intervals= [Interval(0,30),Interval(5,10),Interval(15,20)]
+canAttend=Interval.canAttendMeetins(intervals)
+# intervals = [[0, 30], [5, 10], [15, 20]]
+# scheduler=MeetingScheduler(intervals)
+# canAttend=scheduler.canAttendMeetins()
+print(canAttend)
