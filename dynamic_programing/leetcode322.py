191010fb1af342c403a76faa4fba24951e94e366
vivek kumar <vickythebest008@gmail.com>
2024-12-14 18:39:20 -0800

    dp

--- dynamic_programing/leetcode322.py
+++ dynamic_programing/leetcode322.py
@@ -17,16 +17,18 @@
 # Output: 0
 def coincount(coin,amount):
     result=[1+amount]*(1+amount)
+    print("result: ",result)
     result[0]=0
     for n in range(1,(amount+1)):
         for c in coin:
             if n-c>=0:
                 result[n]=min(result[n],1+result[n-c])
+                print(n, c, " - ",1+result[n-c],"  min result: ",result)
     if result[amount]!=amount:
         return result[amount]
     else:
         return -1
-    # return result[amount] if result[amount] != amount else -1
+    # return result [amount] if result[amount] != amount else -1
 
 nums=[1,2,5]
 print(coincount([1,2,5],11))
